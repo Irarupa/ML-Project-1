62a7c379cd3400865e3d090068a6aac1f21c83f8
Irarupa <62602926+Irarupa@users.noreply.github.com>
2026-01-20 09:21:38 +0530

    Add files via upload

---
+++ kafka.py
@@ -0,0 +1,11 @@
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer(
+    'sensor-topic',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest'
+)
+
+for message in consumer:
+    print(message.value.decode())
+s
